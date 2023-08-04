f1ee028c78abb112a6b7e163ce63db5464e97081
Bartosz Piekarski <bartosz@ravendb.net>
2023-08-04 11:30:22 +0200

    RavenDB-20983 Release and version bump should be done separately from the build

---
+++ publish-stable-release.ps1
@@ -0,0 +1,23 @@
+param(
+    [switch]$DryRun = $false
+)
+
+$ErrorActionPreference = "Stop"
+
+. '.\scripts\getScriptDirectory.ps1'
+. '.\scripts\version.ps1'
+. '.\scripts\githubReleases.ps1'
+
+$PROJECT_DIR = Get-ScriptDirectory
+
+$versionObj = GetVersionInfo
+
+Write-Host $versionObj
+
+$version = $versionObj.Version
+$buildType = $versionObj.BuildType.ToLower()
+
+if ($buildType -eq 'stable') {
+    CreateRelease $version "ravendb" "ravendb" $env:vcsRootBranch $env:GITHUB_ACCESS_TOKEN $env:ravendbChangelog $DryRun
+    BumpVersion $PROJECT_DIR $versionObj.VersionPrefix $buildType $DryRun
+}
